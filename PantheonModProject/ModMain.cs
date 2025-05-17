d355c84d2df111b8c1c5ac73e1a77008781d662a
Joe Irby <jbirby2@gmail.com>
2025-05-16 22:53:54 -0500

    initial commit

---
+++ PantheonModProject/ModMain.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+
+namespace PantheonModProject;
+
+public class ModMain : MelonMod
+{
+    public static string PantheonModProjectSetting;
+    
+    public override void OnInitializeMelon()
+    {
+        var category = MelonPreferences.CreateCategory("PantheonModProject");
+        PantheonModProjectSetting = category.CreateEntry("PantheonModProjectSetting", "PantheonModProjectValue").Value;
+        category.SaveToFile(false);
+
+
+    }
+}
---
+++ PantheonModProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using MelonLoader;
+using PantheonModProject;
+
+// ...
+[assembly: MelonInfo(typeof(ModMain), nameof(PantheonModProject), "1.0.0", "PantheonModProject Author")]
+[assembly: MelonGame("Visionary Realms", "Pantheon")]
